f78f0d7bbe141cc6e008bf5d8203f75e6ad2c8f9
aiyamadeline <aiyaedwards123@gmail.com>
2023-01-24 15:56:29 +0000

    commit

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
+//const { where } = require('sequelize/types');
 const app = express();
 const { Joke } = require('./db');
+const {Op} = require("sequelize")
+const { Sequelize } = require('sequelize');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
-  try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    console.log(req.query)
+    const where = {};
+    
+    if (req.query.tags) {
+      where.tags = {  [Op.like]: `%${req.query.tags}%` }
+    }
+    
+
+    if (req.query.content) {
+      where.tags = {  [Op.like]: `%${req.query.tags}%`}
+    }
+      
+    
+    const jokes = Joke.findAll({where});
     res.send(jokes);
-  } catch (error) {
-    console.error(error);
-    next(error)
-  }
+ 
+  
 });
 
 // we export the app, not listening in here, so that we can run tests
